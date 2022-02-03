e3bcb1c4f0af39ad6b5451cbd77b427a951d39a9
shravanmulge <shravanmulge@gmail.com>
2022-02-04 00:23:43 +0530

    MultiplesOf3Or5 Brute Force Approach

---
+++ MultiplesOf3Or5.py
@@ -0,0 +1,11 @@
+def MultipleSum(n):
+    sum = 0
+    for i in range(1, n):
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i
+    return sum
+
+
+if __name__ == '__main__':
+    result = MultipleSum(1000)
+    print(result)
